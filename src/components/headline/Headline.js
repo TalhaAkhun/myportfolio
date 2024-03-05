2553c9b3a3c4c6c660c7eb08033a040ff7c745d5
TalhaAkhun <TalhaNajeebAkhun3@gmail.com>
2024-03-05 07:23:04 +0500

    testimonials

--- src/components/headline/Headline.js
+++ src/components/headline/Headline.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { FaEnvelope, FaLinkedin, FaPaperclip, FaGithub, FaMedium } from "react-icons/fa";
-import resume from '../../assets/files/Profile.pdf';
 import './Headline.css';
 
 const Headline = () => {
@@ -14,9 +13,8 @@ const Headline = () => {
       <section id="headline" className="headline-section-container">
         <div className="headline-information" data-aos="fade-right">
           <ul className="headline-icons">
-            <li><a href="https://medium.com/@najeebakhun" target="_blank" rel="noreferrer"><FaMedium /></a></li>
-            <li><a href="https://github.com/AbuTalha3" target="_blank" rel="noreferrer"><FaGithub /></a></li>
-            <li><a href="https://www.linkedin.com/in/najeeb-akhun/" target="_blank" rel="noreferrer"><FaLinkedin /></a></li>
+            <li><a href="https://github.com/TalhaAkhun" target="_blank" rel="noreferrer"><FaGithub /></a></li>
+            <li><a href="https://www.linkedin.com/in/in/talha-akhun3/" target="_blank" rel="noreferrer"><FaLinkedin /></a></li>
           </ul>
           <div className="headline-texts">
             <p>Hi, my name is</p>
